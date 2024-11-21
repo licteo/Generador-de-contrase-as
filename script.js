883ebd579b1f64fea200af414b4933901f9998ae
licteo <134066234+licteo@users.noreply.github.com>
2024-11-21 18:53:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.getElementById('generateBtn').addEventListener('click', generatePassword);
+
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById('passwordOutput').value = password;
+}
